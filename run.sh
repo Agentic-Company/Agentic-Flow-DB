28135e453ed9a49ae2a144e1ad7fb2cdbd0eb913
Kwengang Tankeu <dannickstark@gmail.com>
2025-08-22 16:00:58 +0200

    Add SurrealDB schema backup and update log level
    Added a new SurrealDB schema backup file with table and function definitions. Updated the run.sh script to use 'trace' log level instead of 'debug' for more detailed logging.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export USER=root
 export PASS=root
 
 mkdir mydata # Create a directory to store the database, owned by the current user
-docker run --rm --pull always -p 8000:8000 --user $(id -u) -v $(pwd)/mydata:/mydata surrealdb/surrealdb:latest start --log debug --user $USER --pass $PASS rocksdb:/mydata/mydatabase.db --allow-net 
+docker run --rm --pull always -p 8000:8000 --user $(id -u) -v $(pwd)/mydata:/mydata surrealdb/surrealdb:latest start --log trace --user $USER --pass $PASS rocksdb:/mydata/mydatabase.db --allow-net 
